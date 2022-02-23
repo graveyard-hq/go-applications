42bb22bf634d25f95b5463ae68df290a054feb5c
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-02-23 14:26:23 +0000

    useless

---
+++ day-1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name    string
+	age     int
+	country string
+}
+
+func newUser(name string, age int, country string) *Person {
+	newPer := Person{name: name}
+	newPer = Person{age: age}
+	newPer = Person{country: country}
+	return &newPer
+}
+
+func main() {
+	fmt.Println(Person{
+		name:    "Aelpxy",
+		age:     15,
+		country: "United Kingdom",
+	})
+}
