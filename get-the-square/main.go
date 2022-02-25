41b834ba2e3101356e6a70e830b91be9ee0850f4
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-02-25 09:00:17 +0000

    Create main.go

---
+++ get-the-square/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var userInput int
+	fmt.Println("Get the square!")
+
+	fmt.Println("Enter the number")
+	fmt.Scan(&userInput)
+
+	var doJon = multiply(userInput, userInput)
+
+	fmt.Printf("Square of %v is %v \n", userInput, doJon)
+}
+
+func multiply(first int, second int) int {
+	return first * second
+}
