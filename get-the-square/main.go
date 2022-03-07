277caa99a849399ea3e6c7ae292eb6677d4e5ce5
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-03-07 12:43:48 +0600

    lint?

--- get-the-square/main.go
+++ get-the-square/main.go
@@ -5,12 +5,9 @@ import "fmt"
 func main() {
 	var userInput int
 	fmt.Println("Get the square!")
-
 	fmt.Println("Enter the number")
 	fmt.Scan(&userInput)
-
 	var doJon = multiply(userInput, userInput)
-
 	fmt.Printf("Square of %v is %v \n", userInput, doJon)
 }
 
