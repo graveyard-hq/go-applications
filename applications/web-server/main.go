cf21f8f8e47ec76c9031c770dcce57d103152be3
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-09-17 22:48:37 +0600

    feat: bit more advanced version

--- applications/web-server/main.go
+++ applications/web-server/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"flag"
+	"log"
+	"net/http"
 )
 
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("./public")))
-    log.Fatal(http.ListenAndServe(":3000", nil))
+	port := flag.String("p", "8080", "Port to listen on")
+	folder := flag.String("folder", ".", "Folder to serve")
+	flag.Parse()
+
+	log.Println("Starting...")
+
+	http.Handle("/", http.FileServer(http.Dir(*folder)))
+
+	log.Printf("Listening on http://localhost:%v \n", *port)
+	log.Printf("Serving folder %v \n", *folder)
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
