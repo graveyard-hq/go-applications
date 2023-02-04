3c30e01ea7b290cbdc163903c9964fcb1931dd1a
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-02-04 13:30:13 +0600

    feat(algo): bubblesort

---
+++ algorithms/bubble-sort/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "fmt"
+)
+
+func bubbleSort(arr []int) []int {
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        for j := 0; j < n-i-1; j++ {
+            if arr[j] > arr[j+1] {
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            }
+        }
+    }
+    return arr
+}
+
+func main() {
+    arr := []int{64, 34, 25, 12, 22, 11, 90}
+    fmt.Println("Sorted array is:", bubbleSort(arr))
+}
