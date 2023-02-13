0f250a08c6bfbb9910880ef7355e5e6a8524c70b
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-02-14 00:33:55 +0600

    feat: fibonacci search
    This implementation uses the Fibonacci sequence to determine the size of the sub-array to search in each iteration.

---
+++ algorithms/fibonacci-search/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+)
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+unc FibonacciSearch(arr []int, x int) int {
+	n := len(arr)
+	var fib2 = 0
+	var fib1 = 1
+	var fib = fib1 + fib2
+	for fib < n {
+		fib2 = fib1
+		fib1 = fib
+		fib = fib1 + fib2
+	}
+	var offset = -1
+	for fib > 1 {
+		var i = min(offset + fib2, n - 1)
+		if arr[i] < x {
+			fib = fib1
+			fib1 = fib2
+			fib2 = fib - fib1
+			offset = i
+		} else if arr[i] > x {
+			fib = fib2
+			fib1 = fib1 - fib2
+			fib2 = fib - fib1
+		} else {
+			return i
+		}
+	}
+	if fib1 == 1 && arr[offset+1] == x {
+		return offset + 1
+	}
+	return -1
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	result := FibonacciSearch(arr, 6)
+	fmt.Println("Index of 6:", result)
+}
