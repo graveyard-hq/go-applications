5614e1230258c1c1f75af7f521ebb973f269c123
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2023-04-02 16:15:31 +0100

    feat: optimized

--- applications/pipeline/main.go
+++ applications/pipeline/main.go
@@ -1,42 +1,56 @@
 package main
 
 import (
-        "fmt"
-        "sync"
+	"fmt"
+	"sync"
 )
 
-func generator(numbers chan<- int) {
-        for i := 0; i < 1000; i++ {
-                numbers <- i
-        }
-        close(numbers)
+func generateNumbers(numbers chan<- int, done chan<- bool) {
+	defer close(numbers)
+	for i := 0; i < 1000; i++ {
+		numbers <- i
+	}
+	done <- true
 }
 
-func squarer(numbers <-chan int, squares chan<- int) {
-        for i := range numbers {
-                squares <- i * i
-        }
-        close(squares)
+func squareNumbers(numbers <-chan int, squares chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := range numbers {
+		squares <- i * i
+	}
 }
 
-func printer(squares <-chan int, wg *sync.WaitGroup) {
-        for i := range squares {
-                fmt.Println(i)
-        }
-        wg.Done()
+func printSquares(squares <-chan int, done <-chan bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case square, ok := <-squares:
+			if ok {
+				fmt.Println(square)
+			} else {
+				squares = nil
+			}
+		case <-done:
+			if squares == nil {
+				return
+			}
+		}
+	}
 }
 
 func main() {
-        var wg sync.WaitGroup
-        numbers := make(chan int)
-        squares := make(chan int)
+	numbers := make(chan int)
+	squares := make(chan int)
+	done := make(chan bool)
+	var wg sync.WaitGroup
 
-        wg.Add(1)
-        go printer(squares, &wg)
+	wg.Add(1)
+	go printSquares(squares, done, &wg)
 
-        go squarer(numbers, squares)
+	wg.Add(1)
+	go squareNumbers(numbers, squares, &wg)
 
-        generator(numbers)
+	go generateNumbers(numbers, done)
 
-        wg.Wait()
+	wg.Wait()
 }
